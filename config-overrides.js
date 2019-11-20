703fee77ae605b4247dcbdcde607b3baa6944c58
Jeff <fujie1996wang@163.com>
2019-11-20 21:27:46 +0800

    add test

--- config-overrides.js
+++ config-overrides.js
@@ -1,23 +1,18 @@
-const {
-  override,
-  fixBabelImports,
-  addLessLoader,
-  addDecoratorsLegacy
-} = require('customize-cra');
+const { override, fixBabelImports, addLessLoader, addDecoratorsLegacy } = require("customize-cra");
 
 module.exports = override(
   // 按需加载
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
-    style: true,
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
+    style: true
   }),
   // 自定义主题
   addLessLoader({
     javascriptEnabled: true,
     modifyVars: {
-      '@primary-color': '#1DA57A'
-    },
+      "@primary-color": "#1DA57A"
+    }
   }),
   // 添加装饰器语法
   // yarn add @babel/plugin-proposal-decorators --dev
